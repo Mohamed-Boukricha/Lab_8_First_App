7ac4e9449c5d79e111889c04fa0a271dfce928b9
Mohamed-Boukricha <48123922+Mohamed-Boukricha@users.noreply.github.com>
2020-05-15 12:23:46 +0100

    Update shipping.component.ts

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,3 +1,8 @@
+/* We use this ts script  to manage the app-shipping component. 
+      Create by : Mohamed Boukricha
+      Date : 14/05/2020
+*/
+
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 
