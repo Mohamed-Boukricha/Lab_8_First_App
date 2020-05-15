5d17731cbcffeb3a64d3a2579d9e5fb16ccf7a54
Mohamed-Boukricha <48123922+Mohamed-Boukricha@users.noreply.github.com>
2020-05-15 11:45:49 +0100

    Update product-alerts.component.ts

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,10 @@
+/* <!-- We use this ts script  to handle the product alerts. 
+      Create by : Mohamed Boukricha
+      Date : 14/05/2020
+-->*/
+
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
+import { Input } from '@angular/core'; // import Input from @angular/core.
 import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-product-alerts',
@@ -7,7 +12,7 @@ import { Output, EventEmitter } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-@Input() product;
+@Input() product; // we define a property named product with an @Input() decorator.
 @Output() notify = new EventEmitter();
   constructor() { }
 
